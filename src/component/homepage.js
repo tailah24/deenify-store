fa25ccac33836133aaf89e8559d7518b08438156
Tailah Adebanwo <tailahuno@gmail.com>
2021-04-10 23:59:53 -0400

    created new project and added files for homepage

---
+++ src/component/homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-content'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+
+                </div>
+
+        </div>
+
+        </div>
+
+    </div>
+)
+
+export default Homepage
